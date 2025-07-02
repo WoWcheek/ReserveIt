2f6cb3512a6325b357c50b7a096c973f31755ccd
Savchuk Volodymyr <savchuk.v03@gmail.com>
2025-07-02 15:05:21 +0300

    Extended validation of login dto

--- ReserveIt.BLL/DTOs/LoginDTO.cs
+++ ReserveIt.BLL/DTOs/LoginDTO.cs
@@ -5,8 +5,10 @@ namespace ReserveIt.BLL.DTOs;
 public record LoginDTO
 {
     [Required]
+    [StringLength(100, MinimumLength = 5)]
     public string Username { get; set; } = string.Empty;
 
     [Required]
+    [StringLength(100, MinimumLength = 5)]
     public string Password { get; set; } = string.Empty;
 }
